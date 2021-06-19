9f2277e3f91e487c11723754f693903f51f1e818
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-06-19 09:13:34 +0400

    metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -5,14 +5,8 @@ apt update -y
 apt upgrade -y
 pkg update -y
 pkg upgrade -y
-apt install curl -y
+apt install curl wget -y
+wget -q https://hax4us.github.io/TermuxBlack/termuxblack.key -O termuxblack.key && apt-key add termuxblack.key 
 curl -sSf https://raw.githubusercontent.com/Jemacivan/metasploit-installer-termux/main/install_metasploit.sh | bash
 rm -rf metasploit.sh
-echo "Metasploit has been installed. You can open it by msfconsole command. Open it now? y/n"
-if y
-then
-msfconsole
-elif n
-then
-cd ~
-clear
+echo "Metasploit has been installed. You can open it by msfconsole command."
