2231d67fda2d8442b99ebc321da504e683497747
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-20 14:08:59 +0300

    metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -3,8 +3,6 @@ echo "Starting Metasploit installation. It takes 10-20 minutes"
 cd ~
 apt update -y
 apt upgrade -y
-pkg update -y
-pkg upgrade -y
 apt install curl wget -y
 wget -q https://hax4us.github.io/TermuxBlack/termuxblack.key -O termuxblack.key && apt-key add termuxblack.key 
 curl -sSf https://raw.githubusercontent.com/Jemacivan/metasploit-installer-termux/main/install_metasploit.sh | bash
